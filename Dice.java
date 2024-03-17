dfd09858ec2864e2f2e1eec72af99127ad980bd2
Nitipon (Tony) Trimaitreepituk <151781509+nitipon-ton@users.noreply.github.com>
2024-03-17 06:49:44 -0400

    minor

--- Dice.java
+++ Dice.java
@@ -3,18 +3,15 @@ public class Dice
 {
 	//Attributes of our dice
 	private int sides;
-	
 	//Constructor that builds the dice
 	public Dice(int s)
 	{
 		sides=s;
 	}
 	//End Constructor
-	
 	//Methods for Dice
 	public int roll()
 	{
 		return (int)((Math.random()*sides))+1;
 	}
-	
 }
