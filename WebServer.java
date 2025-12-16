db17b23c216d48769152a55a6256e2f7070d614d
Nitipon Trimaitreepituk <iamton2547@gmail.com>
2025-12-15 20:17:24 -0500

    Tiny change

--- WebServer.java
+++ WebServer.java
@@ -8,7 +8,8 @@ import java.io.PrintStream;
 public class WebServer {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newFixedThreadPool(2));
 
         // favicon
